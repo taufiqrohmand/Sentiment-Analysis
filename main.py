a91f589432b6d7b13025778c04ea0cc3e3285cf5
taufiqrohmand <taufiqrohmand@gmail.com>
2023-05-03 11:19:40 +0700

    up

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ app = Flask(__name__)
 app.json_encoder = LazyJSONEncoder
 swagger_template = dict(
     info = {
-    'title': LazyString(lambda:'Challenge Platinum API Documentation and Modelling'),
+    'title': LazyString(lambda:'Challenge Platinum API Documentation and Modelling Machine Learning'),
     'version' : LazyString(lambda: '2.0.0'),
-    'description' : LazyString(lambda: 'Data Documentation API for Sentiment Prediction'),
+    'description' : LazyString(lambda: 'Data Documentation API for Sentiment Prediction Machine Learning'),
     },
     host = LazyString(lambda: request.host)
 )
@@ -44,9 +44,23 @@ swagger = Swagger(app,template = swagger_template, config = swagger_config)
 #Route Home Page
 @app.route('/')
 def hello():
-    return """<h1>Home Page API for Sentiment Prediction</h1>
+    return """<h1>Home Page API for Sentiment Prediction based on Machine Learning Nural Network</h1>
     <p>Silakan untuk masuk ke fitur API bisa dengan  
     <a href = 'http://127.0.0.1:5000/docs'>klik disini</a> </p>"""
+
+#API Text Processing Neural Network
+@swag_from("docs/text_neuralnetwork.yml", methods=['POST'])
+@app.route('/text-Processing', methods=['POST'])
+def text_processing():
+    textinput = request.form.get('text')
+    textoutput = clean(textinput)
+     
+    json_respon = {
+        'input' : textinput,
+        'output' : textoutput,
+    }
+    response_data = jsonify(json_respon)
+    return response_data
 
 
 if __name__ == '__main__' :
